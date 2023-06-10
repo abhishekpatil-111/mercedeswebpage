0c5e08f2815bd086ab91daf3e72b83c3345bfc93
Abhishek Patil <127799639+abhishekpatil-111@users.noreply.github.com>
2023-06-10 22:41:05 +0530

    added index.js

---
+++ js/resp.js
@@ -0,0 +1,14 @@
+// hamburger menu js
+const burger = document.querySelector('.burger');
+const navbar = document.querySelector('.navbar');
+const navList = document.querySelector('.nav-list');
+const rightnav = document.querySelector('.rightnav');
+
+burger.addEventListener('click', () => {
+   rightnav.classList.toggle('v-class-resp');
+   navList.classList.toggle('v-class-resp');
+   navbar.classList.toggle('h-nav-resp');
+});
+
+
+
